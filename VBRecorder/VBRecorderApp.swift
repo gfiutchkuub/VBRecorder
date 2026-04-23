b16ed19ea1b2f470677bb8218f55e285310c8af7
gfiutchkuub <2019302070359@whu.edu.cn>
2026-04-23 20:13:04 +0800

    Initial Commit

---
+++ VBRecorder/VBRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VBRecorderApp.swift
+//  VBRecorder
+//
+//  Created by qiaqia on 2026/4/23.
+//
+
+import SwiftUI
+
+@main
+struct VBRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
